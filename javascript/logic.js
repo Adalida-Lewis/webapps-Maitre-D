01d20be6dbaad2200d6428240b678443ca751cf5
Adalida-Lewis <42812369+Adalida-Lewis@users.noreply.github.com>
2020-02-17 15:24:53 -0600

    Initial commit

---
+++ javascript/logic.js
@@ -0,0 +1,32 @@
+const totalInput = document.querySelector("#total");
+const tipNum = document.querySelector("#tip");
+const rateInput = document.querySelector("#rate");
+const taxNum = document.querySelector("#tax");
+const grandTotalNum = document.querySelector("#grand-total");
+const calculateButton = document.querySelector("#calc");
+
+calculateButton.onclick = async function(){
+    let calculateTotal = totalInput.value;
+    let calculateRate = rateInput.value;
+    let totalNum = parseInt(calculateTotal);
+    let rateNum = parseInt(calculateRate);
+    
+    let tipnum2 = await calcTip(totalNum, rateNum);
+    tipNum.innerHTML = tipnum2;
+    
+    const taxnum2 = await calcTax(totalNum, rateNum);
+    taxNum.innerHTML = taxnum2;
+    
+    grandTotalNum.innerHTML = parseInt(calculateTotal)+parseInt(tipnum2)+parseFloat(taxnum2);
+    
+}
+
+const calcTip = async function (totalInput, rateNum){
+    let percent = rateNum/100;
+    let tip = totalInput * percent;
+    return tip.toFixed(2);
+}
+const calcTax = async function (totalInput){
+    let tax = totalInput * 0.055;
+    return tax.toFixed(2);
+}
